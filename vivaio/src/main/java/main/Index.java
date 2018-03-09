913c501e12b033e2d09b5c2da9a0f8490e7958c3
Tommy <tomganguz@gmail.com>
2018-03-09 21:08:08 +0100

    Prova utilizzo eclipse per git

--- vivaio/src/main/java/main/Index.java
+++ vivaio/src/main/java/main/Index.java
@@ -13,6 +13,8 @@ public class Index extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		String prova;
+		
         resp.setContentType("text/html");
 				
         String nextJSP = "/index.jsp";
